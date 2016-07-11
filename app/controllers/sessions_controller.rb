8c1eb6ecc18d9f4c02b3d79374422c9fa72566e8
snlamm <shmuel.lamm@gmail.com>
2016-07-11 17:00:24 -0400

    config login controller action and add alerts partial to login view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: session_params[:username])
+    if @user && @user.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      # redirect_to root_path
+    else
+      flash["alert"] = "Login failed."
+      render :new
+    end
   end
 
   def destroy
+    
+  end
+
+  private
+  def session_params
+    params.require(:user).permit(:username, :password)
   end
 end
